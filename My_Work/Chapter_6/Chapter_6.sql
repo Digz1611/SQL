88038d79b9d773e44c4589a41a0abf9199d6ead8
Digz1611 <diego.langeveldt@gmail.com>
2024-08-01 11:41:09 +0200

    7 done

--- My_Work/Chapter_6/Chapter_6.sql
+++ My_Work/Chapter_6/Chapter_6.sql
@@ -174,7 +174,7 @@ CREATE TABLE us_counties_2000 (
 );
 
 COPY us_counties_2000
-FROM 'C:\YourDirectory\us_counties_2000.csv'
+FROM 'C:\Code_College\Java_Bootcamp\SQL\My_Work\Chapter_6\us_counties_2000.csv'
 WITH (FORMAT CSV, HEADER);
 
 SELECT c2010.geo_name,
