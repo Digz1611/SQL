22284b5638a30baee0655a2f3958e8f8072fc72e
Digz1611 <diego.langeveldt@gmail.com>
2024-08-05 13:18:38 +0200

    done with 9

--- My_Work/Chapter_8/Chapter_8.sql
+++ My_Work/Chapter_8/Chapter_8.sql
@@ -1,12 +1,6 @@
---------------------------------------------------------------
--- Practical SQL: A Beginner's Guide to Storytelling with Data
--- by Anthony DeBarros
-
--- Chapter 8 Code Examples
---------------------------------------------------------------
+-- Chapter 8
 
 -- Listing 8-1: Creating and filling the 2014 Public Libraries Survey table
-
 CREATE TABLE pls_fy2014_pupld14a (
     stabr varchar(2) NOT NULL,
     fscskey varchar(6) CONSTRAINT fscskey2014_key PRIMARY KEY,
@@ -88,11 +82,11 @@ CREATE INDEX city2014_idx ON pls_fy2014_pupld14a (city);
 CREATE INDEX visits2014_idx ON pls_fy2014_pupld14a (visits);
 
 COPY pls_fy2014_pupld14a
-FROM 'C:\YourDirectory\pls_fy2014_pupld14a.csv'
+FROM 'C:\Code_College\Java_Bootcamp\SQL\My_Work\Chapter_8\pls_fy2014_pupld14a.csv'
 WITH (FORMAT CSV, HEADER);
 
--- Listing 8-2: Creating and filling the 2009 Public Libraries Survey table
 
+-- Listing 8-2: Creating and filling the 2009 Public Libraries Survey table
 CREATE TABLE pls_fy2009_pupld09a (
     stabr varchar(2) NOT NULL,
     fscskey varchar(6) CONSTRAINT fscskey2009_key PRIMARY KEY,
@@ -170,24 +164,24 @@ CREATE INDEX city2009_idx ON pls_fy2009_pupld09a (city);
 CREATE INDEX visits2009_idx ON pls_fy2009_pupld09a (visits);
 
 COPY pls_fy2009_pupld09a
-FROM 'C:\YourDirectory\pls_fy2009_pupld09a.csv'
+FROM 'C:\Code_College\Java_Bootcamp\SQL\My_Work\Chapter_8\pls_fy2009_pupld09a.csv'
 WITH (FORMAT CSV, HEADER);
 
--- Listing 8-3: Using count() for table row counts
 
+-- Listing 8-3: Using count() for table row counts
 SELECT count(*)
 FROM pls_fy2014_pupld14a;
 
 SELECT count(*)
 FROM pls_fy2009_pupld09a;
 
--- Listing 8-4: Using count() for the number of values in a column
 
+-- Listing 8-4: Using count() for the number of values in a column
 SELECT count(salaries)
 FROM pls_fy2014_pupld14a;
 
--- Listing 8-5: Using count() for the number of distinct values in a column
 
+-- Listing 8-5: Using count() for the number of distinct values in a column
 SELECT count(libname)
 FROM pls_fy2014_pupld14a;
 
@@ -205,12 +199,13 @@ SELECT libname, city, stabr
 FROM pls_fy2014_pupld14a
 WHERE libname = 'OXFORD PUBLIC LIBRARY';
 
+
 -- Listing 8-6: Finding the most and fewest visits using max() and min()
 SELECT max(visits), min(visits)
 FROM pls_fy2014_pupld14a;
 
--- Listing 8-7: Using GROUP BY on the stabr column
 
+-- Listing 8-7: Using GROUP BY on the stabr column
 -- There are 56 in 2014.
 SELECT stabr
 FROM pls_fy2014_pupld14a
@@ -223,8 +218,8 @@ FROM pls_fy2009_pupld09a
 GROUP BY stabr
 ORDER BY stabr;
 
--- Listing 8-8: Using GROUP BY on the city and stabr columns
 
+-- Listing 8-8: Using GROUP BY on the city and stabr columns
 SELECT city, stabr
 FROM pls_fy2014_pupld14a
 GROUP BY city, stabr
@@ -236,23 +231,23 @@ FROM pls_fy2014_pupld14a
 GROUP BY city, stabr
 ORDER BY count(*) DESC;
 
--- Listing 8-9: GROUP BY with count() on the stabr column
 
+-- Listing 8-9: GROUP BY with count() on the stabr column
 SELECT stabr, count(*)
 FROM pls_fy2014_pupld14a
 GROUP BY stabr
 ORDER BY count(*) DESC;
 
--- Listing 8-10: GROUP BY with count() on the stabr and stataddr columns
 
+-- Listing 8-10: GROUP BY with count() on the stabr and stataddr columns
 SELECT stabr, stataddr, count(*)
 FROM pls_fy2014_pupld14a
 GROUP BY stabr, stataddr
 ORDER BY stabr ASC, count(*) DESC;
 
+
 -- Listing 8-11: Using the sum() aggregate function to total visits to
 -- libraries in 2014 and 2009
-
 -- 2014
 SELECT sum(visits) AS visits_2014
 FROM pls_fy2014_pupld14a
@@ -263,16 +258,16 @@ SELECT sum(visits) AS visits_2009
 FROM pls_fy2009_pupld09a
 WHERE visits >= 0;
 
--- Listing 8-12: Using sum() to total visits on joined 2014 and 2009 library tables
 
+-- Listing 8-12: Using sum() to total visits on joined 2014 and 2009 library tables
 SELECT sum(pls14.visits) AS visits_2014,
        sum(pls09.visits) AS visits_2009
 FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
 ON pls14.fscskey = pls09.fscskey
 WHERE pls14.visits >= 0 AND pls09.visits >= 0;
 
--- Listing 8-13: Using GROUP BY to track percent change in library visits by state
 
+-- Listing 8-13: Using GROUP BY to track percent change in library visits by state
 SELECT pls14.stabr,
        sum(pls14.visits) AS visits_2014,
        sum(pls09.visits) AS visits_2009,
@@ -284,8 +279,8 @@ WHERE pls14.visits >= 0 AND pls09.visits >= 0
 GROUP BY pls14.stabr
 ORDER BY pct_change DESC;
 
--- Listing 8-14: Using HAVING to filter the results of an aggregate query
 
+-- Listing 8-14: Using HAVING to filter the results of an aggregate query
 SELECT pls14.stabr,
        sum(pls14.visits) AS visits_2014,
        sum(pls09.visits) AS visits_2009,
@@ -297,3 +292,5 @@ WHERE pls14.visits >= 0 AND pls09.visits >= 0
 GROUP BY pls14.stabr
 HAVING sum(pls14.visits) > 50000000
 ORDER BY pct_change DESC;
+
+
